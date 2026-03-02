825ad8ac3b3ebe0fa18b9d6f80c16f8a105faf78
Jared Kirchgatter <ryoucerious.dev@gmail.com>
2026-03-01 23:32:40 -0700

    chore: bump version to 1.0.3

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -314,7 +314,7 @@ export class SettingsPageComponent {
   }
 
   getAppVersion() {
-    return environment.version || '1.0.2';
+    return environment.version || '1.0.3';
   }
 
   getPlatform() {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  version: '1.0.2'
+  version: '1.0.3'
 };
